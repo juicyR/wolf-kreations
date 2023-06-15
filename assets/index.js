9dd4c66f40b45f9a14c03fd71d60294d6347ad97
Reginald Matovu <49374492+juicyR@users.noreply.github.com>
2023-06-15 15:05:05 +0300

    first commit

---
+++ assets/index.js
@@ -0,0 +1,41 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("banner-image");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var headerImage = document.querySelectorAll(".slide-image");
+var imgNum = 0;
+
+function colourChangeIn(imgNum) {
+  headerImage[imgNum].style.filter = "grayscale(0%)";
+  console.log("entered");
+}
+
+function colourChangeOut(imgNum) {
+  headerImage[imgNum].style.filter = "grayscale(100%)";
+  console.log("left");
+}
